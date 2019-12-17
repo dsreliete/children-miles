eb9c2506554c2afdb8b414adf45f61aca0c1bb96
dsreliete <dsreliete@gmail.com>
2019-12-17 09:42:34 -0500

    change styles Addeditdelcardcomponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import AddEditDelCard from './AddEditDelCardComponent';
+import ChildrenList from './ChildrenListComponent';
 /* Customized from: https://github.com/gauravchl/react-simple-sidenav */
 class Main extends Component {
 
@@ -8,6 +9,7 @@ class Main extends Component {
         return(
             <div>
                 <Header />
+                <ChildrenList />
                 <AddEditDelCard />
             </div>
         );
