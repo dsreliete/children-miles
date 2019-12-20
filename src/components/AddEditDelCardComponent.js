35bca2a2981babe74550e719b5106a04169d0fff
dsreliete <dsreliete@gmail.com>
2019-12-20 15:27:44 -0500

    changed add edit del button component and created list item

--- src/components/AddEditDelCardComponent.js
+++ src/components/AddEditDelCardComponent.js
@@ -2,25 +2,23 @@ import React from 'react';
 
 export default function AddEditDelCard(props) {
     return(
-        <div className="m-5">
-            <h1><i className="fa fa-user fa-1x mr-3"></i>Children</h1>
-            <hr/>
-            <div className="card-deck justify-content-center align-items-center mt-4">
-                <div onClick={() => {alert("Hello from here")}}>
+        <div className="container-buttons">
+            <div className="row justify-content-center align-items-center">
+                <div className="col-sm-4" onClick={() => {alert("Hello from here")}}>
                     <div className="card card-button border-success text-center d-flex flex-column justify-content-center">
-                        <i className="fa fa-plus-square fa-4x p-5"></i>
+                        <i className="fa fa-plus-square fa-3x p-4"></i>
                     </div>
                 </div>    
 
-                <div onClick={() => {alert("Hello from here")}}>
+                <div className="col-sm-4" onClick={() => {alert("Hello from here")}}>
                     <div className="card card-button border-success text-center d-flex flex-column justify-content-center">
-                        <i className="fa fa-edit fa-4x p-5"></i>
+                        <i className="fa fa-edit fa-3x p-4"></i>
                     </div>
                 </div>
 
-                <div onClick={() => {alert("Hello from here")}}>
+                <div className="col-sm-4" onClick={() => {alert("Hello from here")}}>
                     <div className="card card-button border-success text-center d-flex flex-column justify-content-center">
-                        <i className="fa fa-trash fa-4x p-5"></i>
+                        <i className="fa fa-trash fa-3x p-4"></i>
                     </div>
                 </div>
             </div>
--- src/components/ChildrenListComponent.js
+++ src/components/ChildrenListComponent.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import { Card } from 'reactstrap';
+
 
 export default function ChildrenList(props) {
-    return(<div></div>);
+    return(
+            <div className="container-wrapper">
+                <div className="row">
+                    <div className="col-sm-12 mb-5">
+                        <h1><i className="fa fa-user fa-1x mr-3"></i>Children</h1>
+                        <hr/>
+                    </div>
+                </div>       
+                <div class="row justify-content-center">
+                    <div className="col-md-6 mb-3">
+                        <Card className="p-4">
+                            <div className="media">
+                                <img className="img" src="assets/images/joao.jpg" alt="child" />
+                                <div className="media-body ml-4 align-self-center">
+                                    <h4>Sheldon Cooper</h4>
+                                    <h6>6 anos</h6>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                    <div className="col-md-6 mb-3">
+                        <Card className="p-4">
+                            <div className="media">
+                                <img className="img" src="assets/images/maria.jpg" alt="child" />
+                                <div className="media-body ml-4 align-self-center">
+                                    <h4>Amy Farah Fowler</h4>
+                                    <h6>11 anos</h6>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+    );
 }
