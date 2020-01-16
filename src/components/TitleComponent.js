69d3582c181eb519731149ab9b0871c893c425bd
dsreliete <dsreliete@gmail.com>
2020-01-16 17:15:44 -0500

    changed title size

--- src/components/TitleComponent.js
+++ src/components/TitleComponent.js
@@ -5,7 +5,7 @@ export default class TitleComponent extends Component {
     render(){
         return (
             <div className="col-sm-12 title-container">
-                <h1><i className="fa fa-user fa-1x mr-3"></i>{this.props.title}</h1>
+                <h3><i className="fa fa-user fa-1x mr-3"></i>{this.props.title}</h3>
                 <i className={this.props.icon} onClick={this.props.showAddComponent}></i>
                 <hr/>
             </div>
