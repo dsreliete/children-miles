9d807be1e83a5c447a8ac2ef25bd71f2567a9c50
dsreliete <dsreliete@gmail.com>
2020-01-16 17:18:00 -0500

    ädded list item button to edit and delete child

--- src/components/ChildrenItem.js
+++ src/components/ChildrenItem.js
@@ -10,10 +10,25 @@ export default function ChildrenItem(props) {
                     <img className="img" src={imageSrc} alt="child" />
                     <div className="media-body ml-4 align-self-center">
                         <p>{child.name}</p>
-                        <p>{child.birthDate.toISOString()}</p>
+                        {/* <p>{child.birthDate.toISOString()}</p> */}
+                        <p>{child.birthDate}</p>
+                    </div>
+                    <div className="col-2 justify-content-end align-self-center" onClick={() => editChild(child)}>
+                        <i className="fa fa-edit fa-2x"></i>
+                    </div>
+                    <div className="col-2 justify-content-end align-self-center" onClick={() => deleteChild(child)}>
+                        <i className="fa fa-trash fa-2x"></i>
                     </div>
                 </div>
             </div>
         </div>
     );
+}
+
+const editChild = (child) => {
+    console.log("edit " + child.name)
+}
+
+const deleteChild = (child) => {
+    console.log("delete " + child.name)
 }
