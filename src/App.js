92ef06bb9944506fdb694fbcb6c6e3d177b87ca6
dsreliete <dsreliete@gmail.com>
2020-03-22 21:45:45 -0400

    changed ui strategy2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 import Main from './components/MainComponent';
 
+import { Provider } from 'react-redux';
+// import { ConfigureStore } from './redux/configureStore';
+
+// const store = ConfigureStore();
+
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Main />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
