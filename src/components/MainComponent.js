a6147f720b1d39c2e005dacde038ebcad04aa848
dsreliete <dsreliete@gmail.com>
2020-03-23 02:21:55 -0400

    added first version header new version and router configuration

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,47 @@
 import React from 'react';
-// import Header from './HeaderComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+
+import Header from './HeaderComponent.js';
+// import Home from './HomeComponent.js';
+import Family from './family/FamilyComponent.js';
+import Child from './children/ChildrenComponent.js';
+// import Goal from './goal/GoalComponent.js';
+// import Penalty from './penalty/PenaltyComponent.js';
+// import Award from './award/AwardComponent.js';
+// import History from './HistoryComponent.js';
+
 
 const Main = (props) => {
 
+    // const HomePage = () => {
+    //     return (
+    //         <Home />
+    //     )
+    // }
+
+    const FamilyPage = () => {
+        return (
+            <Family />
+        )
+
+    }
+
+    const ChildPage = () => {
+        return (
+            <Child />
+        )
+    }
+
+
     return (
         <div>
-            {/* <Header /> */}
+            <Header />
+            <Switch>
+                {/* <Route path='/home' component={HomePage} /> */}
+                <Route path='/family' component={FamilyPage} />
+                <Route path='/child' component={ChildPage} />
+                <Redirect to='/child' />
+            </Switch>
         </div>
     );
 }
