7d4c9d26ee5a4f6e383e4f4ff2113ba5853d357b
dsreliete <dsreliete@gmail.com>
2020-04-03 22:48:10 -0400

    üpdate MainComponent routes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
-import Signup from './auth/SignupComponent';
 import Home from './HomeComponent.js';
 import Family from './family/FamilyComponent.js';
 import Child from './children/ChildrenComponent.js';
@@ -9,37 +8,49 @@ import Goal from './goals/GoalComponent.js';
 import Penalty from './penalties/PenaltyComponent.js';
 import Award from './awards/AwardComponent.js';
 import History from './HistoryComponent.js';
+
+import Signup from './auth/SignupComponent';
 import VerifyEmail from './auth/VerifyEmailComponent';
 import ResendEmail from './auth/ResendEmailComponent';
 import Signin from './auth/SigninComponent';
 import RescuePassword from './auth/RescuePasswordComponent';
 import UpdatePassword from './auth/UpdatePasswordComponent';
 
+// import requireAuth from './hoc/RequireAuth';
+// import requireNotAuth from './hoc/RequireNotAuth';
+
 const Main = (props) => {
+
+    const VerifyEmailAndAuthUser = () => {
+        return <VerifyEmail token={props} />
+    }
+
+    const VerifyEmailAndUpdatePassword = () => {
+        return <UpdatePassword token={props} />
+    }
+
+    const ResendEmailToAuthUser = ({match}) => {
+        return <ResendEmail email={match.params.email} />
+    }
 
     return (
         <div>
             <Switch>
-                <Route path='/home' render={() => <Home/>} />
-                <Route path='/family' render={() => <Family/>} />
-                <Route path='/child' render={() => <Child/>} />
-                <Route path='/awards' render={() => <Award/>} />
-                <Route path='/goals' render={() => <Goal/>} />
-                <Route path='/penalties' render={() => <Penalty/>} />
-                <Route path='/history' render={() => <History/>}/>
-                <Route path='/signup' render={() => <Signup/>} />
-                <Route path='/verifyEmail/:token'  
-                    render={(props) => <VerifyEmail token={props}/>}
-                />
-                <Route path='/resendEmail/:email'   
-                    render={({match}) => (<ResendEmail email={match.params.email}/>)}
-                />
-                <Route path='/signin' render={() => <Signin/>} />
-                <Route path='/rescueCredentials' render={() => <RescuePassword />} />
-                <Route path='/resetCredentials/:token' 
-                    render={(props) => <UpdatePassword token={props} />} 
-                />
+                <Route path='/home' component={Home} />
+                <Route path='/family' component={Family} />
+                <Route path='/child' component={Child} />
+                <Route path='/awards' component={Award} />
+                <Route path='/goals' component={Goal} />
+                <Route path='/penalties' component={Penalty} />
+                <Route path='/history' component={History} />
 
+                <Route path='/signup' component={Signup} />
+                <Route path='/signin' component={Signin} />
+                <Route path='/rescueCredentials' component={RescuePassword} />
+                <Route path='/verifyEmail/:token' component={VerifyEmailAndAuthUser} />
+                <Route path='/resendEmail/:email' component={ResendEmailToAuthUser} />
+                <Route path='/resetCredentials/:token' component={VerifyEmailAndUpdatePassword} />
+                
                 <Redirect to='/signin' />
             </Switch>
         </div>
